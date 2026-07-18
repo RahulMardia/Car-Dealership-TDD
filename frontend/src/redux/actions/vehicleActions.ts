a7d615b1ea9314a439a6c6dba02944ba4724be12
rahulmardia <rahulmardia999@gmail.com>
2026-07-18 23:42:29 +0530

    Vehicle Action to Purchase Vehicle and css fix in the root file

--- frontend/src/redux/actions/vehicleActions.ts
+++ frontend/src/redux/actions/vehicleActions.ts
@@ -1,9 +1,9 @@
 import { makeAPIRequest } from "@/lib/apiService";
 import { IS_LOADING, SET_ERROR, SET_VEHICLES } from "../actionTypes";
-import { api, GET } from "@/lib/apiConstants";
+import { api, GET, POST } from "@/lib/apiConstants";
 import { AppDispatch } from "../store";
 
-export const getVehicles = () => async (dispatch: AppDispatch) => {
+export const getVehicles = (params?: any) => async (dispatch: AppDispatch) => {
   try {
     dispatch({ type: IS_LOADING, payload: true });
     dispatch({ type: SET_ERROR, payload: null });
@@ -11,9 +11,40 @@ export const getVehicles = () => async (dispatch: AppDispatch) => {
     const response: any = await makeAPIRequest({
       method: GET,
       url: api.vehicles,
-    
+      params: params
     });
 
+    if (response.success) {
+        
+      dispatch({ type: SET_VEHICLES, payload: response?.data });
+      return response?.data;
+    }
+  } catch (error: any) {
+    const errorMessage = error?.response?.data?.message || "Login failed";
+    dispatch({ type: SET_ERROR, payload: errorMessage });
+    return false;
+  } finally {
+    dispatch({ type: IS_LOADING, payload: false });
+  }
+};
+
+
+export const purchaseVehicle = (vehicleId: string, quantity: number) => async (dispatch: AppDispatch) => {
+  try {
+    dispatch({ type: IS_LOADING, payload: true });
+    dispatch({ type: SET_ERROR, payload: null });
+
+    const response: any = await makeAPIRequest({
+      method: POST,
+      url: `${api.vehicles}/${vehicleId}/purchase`,
+      data: {
+    quantityToPurchase: quantity,
+  },
+    });
+    console.log('Quantity->',quantity);
+    
+    console.log('Response->',response);
+    
     if (response.success) {
         
       dispatch({ type: SET_VEHICLES, payload: response?.data });
